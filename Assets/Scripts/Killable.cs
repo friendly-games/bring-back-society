1478e401f66a2a5f09af4959d8f42db41c43a223
Mackenzie Zastrow <zastrowm+github@gmail.com>
2014-04-10 21:17:12 -0500

    Initial Commit

---
+++ Assets/Scripts/Killable.cs
@@ -0,0 +1,38 @@
+﻿using Behavior;
+using UnityEngine;
+
+/// <summary> MonoBehavoir for a killable object. </summary>
+public class Killable : MonoBehaviour, IKillable
+{
+  /// <summary> The maximum health amount of health for this entity. </summary>
+  public int maxHealth = 100;
+
+  /// <summary> The resistance. </summary>
+  public Resistance resistance;
+
+  Resistance IKillable.Resistance
+  {
+    get { return resistance; }
+  }
+
+  /// <summary> The maximum amount of health that the entity can have. </summary>
+  int IKillable.MaxHealth
+  {
+    get { return maxHealth; }
+  }
+
+  /// <summary> The health of the object. </summary>
+  public int Health { get; set; }
+
+  /// <inheritdoc />
+  public void Start()
+  {
+    this.Initialize();
+  }
+
+  /// <inheritdoc />
+  void IKillable.Destroy()
+  {
+    Destroy(gameObject);
+  }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Behavior;
+using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour
+{
+  public Light LightSource;
+  public AudioSource AudioSource;
+  private Transform _parentTransform;
+
+  // Use this for initialization
+  public void Start()
+  {
+    _parentTransform = transform.parent.transform;
+
+    LightSource = GetComponentsInChildren<Light>().First();
+    AudioSource = GetComponentsInChildren<AudioSource>().First();
+  }
+
+  public void Fire()
+  {
+    StartCoroutine(FireWeapon());
+  }
+
+  private IEnumerator FireWeapon()
+  {
+    LightSource.enabled = true;
+    AudioSource.Play();
+
+    RaycastHit hitInfo;
+    if (Physics.Raycast(new Ray(_parentTransform.position, _parentTransform.forward), out hitInfo, 100))
+    {
+      var otherObject = hitInfo.collider.gameObject;
+      var killable = otherObject.Get<IKillable>();
+      if (killable != null)
+      {
+        Debug.Log("Hit Enemy");
+        killable.Damage(50);
+      }
+      else
+      {
+        Debug.Log("Enemy missed");
+      }
+    }
+
+    yield return new WaitForSeconds(0.05f);
+    LightSource.enabled = false;
+  }
+}
