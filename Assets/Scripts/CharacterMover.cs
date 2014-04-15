3143ced94244c8477d2d555ec74e3acf3cc83f85
Mackenzie Zastrow <zastrowm+github@gmail.com>
2014-04-14 20:06:26 -0500

    Implement character jumping

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -10,21 +10,29 @@ internal class CharacterMover : MonoBehaviour
 
   public float MaxRotation = 10f;
 
+  public float JumpSpeed = 1.0f;
+
   [Range(0.0f, 1.0f)]
   public float FollowDrag = 0.2f;
 
   private Rigidbody _rigidbody;
   private Transform _transform;
+  private Rigidbody _ground;
+  private DateTime _lastJumpTime;
 
   /// <summary> The target velocity of the item. </summary>
   public Vector3 TargetVelocity { get; set; }
 
   public Quaternion TargetRotation { get; set; }
 
+  /// <summary> True if the character should be jumping if possible.  </summary>
+  public bool AttemptJump { get; set; }
+
   public void Start()
   {
     _rigidbody = GetComponent<Rigidbody>();
     _transform = GetComponent<Transform>();
+    _ground = GameObject.Find("Ground").rigidbody;
 
     TargetRotation = _transform.rotation;
   }
@@ -36,7 +44,22 @@ internal class CharacterMover : MonoBehaviour
     targetVelocity.y = 0;
     currentVelocity.y = 0;
 
-    var diffVelocity = (targetVelocity*MaxSpeed) - currentVelocity;
+    targetVelocity *= MaxSpeed;
+
+    var diffVelocity = targetVelocity - currentVelocity;
+
+    if (AttemptJump && DateTime.Now - _lastJumpTime > TimeSpan.FromMilliseconds(100))
+    {
+      // TODO replace constants
+      var ray = new Ray(_transform.position, Vector3.down);
+      RaycastHit hitInfo;
+      if (_ground.collider.Raycast(ray, out hitInfo, 1.05f))
+      {
+        diffVelocity.y = JumpSpeed;
+        _lastJumpTime = DateTime.Now;
+      }
+    }
+
     _rigidbody.AddForce(diffVelocity, ForceMode.Impulse);
 
     _transform.rotation = Quaternion.Slerp(_transform.rotation,
