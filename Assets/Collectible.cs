bb85ae1f98a8b9f1d8c807846855717eeac0c479
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2014-06-01 20:52:27 -0500

    Apply rotation to pickup item.

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -2,12 +2,23 @@
 using BringBackSociety.Controllers;
 using UnityEngine;
 
-internal class Collectible : MonoBehaviour, IStart, IObjectProvider<InventoryStack>
+internal class Collectible : MonoBehaviour, IObjectProvider<InventoryStack>, IStartAndUpdate
 {
+  private Transform _transform;
+
+  /// <inheritdoc />
   public void Start()
   {
     // TODO modify this so that it does not initialize always the same
     Instance = new InventoryStack(GlobalResources.Ammos[0], 10);
+
+    _transform = transform;
+  }
+
+  /// <inheritdoc />
+  public void Update()
+  {
+    _transform.Rotate(0, 60 * Time.deltaTime, 0);
   }
 
   /// <summary> The item to be given to the actor who takes the collectible. </summary>
