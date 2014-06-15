28a9367173884d14005ebc0bea6068e5268d90f6
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2014-06-15 14:02:30 -0500

    Move test files under assets folder

--- Assets/BringBackSociety/Tests/Properties/AssemblyInfo.cs
+++ Assets/BringBackSociety/Tests/Properties/AssemblyInfo.cs
@@ -2,40 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Grid")]
+[assembly: AssemblyTitle("Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Grid")]
+[assembly: AssemblyProduct("Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("5ecbe5d8-ee30-49df-8bb7-10371e744767")]
+[assembly: Guid("ce77166c-a51d-467e-be85-7a4e6fa824a7")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Tests")]
-[assembly: InternalsVisibleTo("Assembly-CSharp")]
---
+++ Assets/BringBackSociety/Tests/Tasks/CoroutineDispatcherTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using BringBackSociety.Tasks;
+using Xunit;
+
+namespace Tests.Tasks
+{
+  public class CoroutineDispatcherTests
+  {
+    private readonly CoroutineDispatcher _dispatcher;
+    private readonly Coroutine _okayCoroutine;
+    private readonly Coroutine _waitCoroutine;
+    private readonly Coroutine _exceptionCoroutine;
+
+    public CoroutineDispatcherTests()
+    {
+      _dispatcher = new CoroutineDispatcher();
+      _okayCoroutine = new Coroutine(OkayEnumerator());
+      _waitCoroutine = new Coroutine(WaitForCoroutine());
+      _exceptionCoroutine = new Coroutine(Exceptions());
+    }
+
+    public void Run(int iterations = -1)
+    {
+      if (iterations < 0)
+      {
+        while (_dispatcher.HasWork)
+        {
+          _dispatcher.Continue();
+        }
+      }
+      else
+      {
+        for (int i = 0; i < iterations; i++)
+        {
+          _dispatcher.Continue();
+        }
+      }
+    }
+
+    [Fact]
+    [Description("Dispatcher works")]
+    public void Dispatcher_works()
+    {
+      _dispatcher.Start(_okayCoroutine);
+
+      Run();
+
+      Assert.True(_okayCoroutine.IsComplete);
+      Assert.False(_dispatcher.HasWork);
+    }
+
+    [Fact]
+    [Description("Waiting works")]
+    public void Waiting_works()
+    {
+      _dispatcher.Start(_waitCoroutine);
+
+      Assert.False(_waitCoroutine.IsComplete);
+      Run(14);
+
+      // should not be done
+      Assert.False(_waitCoroutine.IsComplete);
+      Assert.False(_okayCoroutine.IsComplete);
+
+      // okay should be done, wait should not
+      Run(1);
+      Assert.True(_okayCoroutine.IsComplete);
+      Assert.False(_waitCoroutine.IsComplete);
+
+      // wait shouldn't yet, we're one step away
+      Run(4);
+      Assert.False(_waitCoroutine.IsComplete);
+
+      // should be good now
+      Run(1);
+      Assert.True(_waitCoroutine.IsComplete);
+
+      Assert.False(_dispatcher.HasWork);
+    }
+
+    [Fact]
+    [Description("Exceptions are propegated")]
+    public void OriginalExceptionIsPropegated()
+    {
+      _dispatcher.Start(_exceptionCoroutine);
+
+      Run(4);
+
+      try
+      {
+        Run(1);
+      }
+      catch (ArgumentException exception)
+      {
+        Console.WriteLine(exception);
+        return;
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine(exception);
+        Assert.False(true, "Incorrect type of exception was thrown");
+      }
+
+      Assert.False(true);
+    }
+
+    private IEnumerator OkayEnumerator()
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        yield return i;
+      }
+    }
+
+    private IEnumerator WaitForCoroutine()
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        if (i == 5)
+        {
+          yield return _okayCoroutine;
+        }
+        else
+        {
+          yield return i;
+        }
+      }
+    }
+
+    private IEnumerator Exceptions()
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        if (i == 5)
+        {
+          throw new ArgumentException("Argument not valid");
+        }
+        else
+        {
+          yield return i;
+        }
+      }
+    }
+  }
+}
