dcf1327537ce649f2c905b9bec0a21f219813ce1
Mackenzie Zastrow <zastrowm+bitbucket@gmail.com>
2014-06-15 17:21:52 -0500

    Fix up the target locator

--- Assets/BringBackSociety/Editor/Tests/AssertEx.cs
+++ Assets/BringBackSociety/Editor/Tests/AssertEx.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Core;
+using NUnit.Framework;
+using UnityEngine;
+using UnityTest;
 
 namespace Assets.BringBackSociety.Editor.Tests
 {
   public static class AssertEx
   {
-
     public static bool IsCloseTo(this float value, float other, float delta = 0.0001f)
     {
       return Math.Abs(value - other) < delta;
     }
 
+    public static void AreEqual(Vector2 expected, Vector2 actual)
+    {
+      Assert.AreEqual(expected.x, actual.x, 0.0001f, "X");
+      Assert.AreEqual(expected.y, actual.y, 0.0001f, "Y");
+    }
   }
 }
---
+++ Assets/BringBackSociety/Editor/Tests/Fixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assets.BringBackSociety.Editor.Tests
+{
+  internal class Fixture
+  {
+    [TestFixtureSetUp]
+    public void SetupFixture()
+    {
+      GlobalSettings.DefaultFloatingPointTolerance = 0.0001;
+    }
+  }
+}
