835d5a4ba230e4b59faccb54fac1b878170d98de
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2014-06-15 13:57:04 -0500

    Move all code files under the assets folder

---
+++ Assets/BringBackSociety/Engine/SnapshotToken.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Annotations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BringBackSociety
+{
+  /// <summary> Represents a value indicating the number of times something has changed. </summary>
+  public struct SnapshotToken : IEquatable<SnapshotToken>
+  {
+    private readonly int _changeCount;
+
+    /// <summary> Constructor. </summary>
+    /// <param name="changeCount"> The id of the current change count. </param>
+    private SnapshotToken(int changeCount)
+    {
+      _changeCount = changeCount;
+    }
+
+    /// <summary> Gets the next change counter in the series. </summary>
+    /// <returns> A ChangeCount representing one more change. </returns>
+    [Pure]
+    public SnapshotToken Next()
+    {
+      return new SnapshotToken(_changeCount + 1);
+    }
+
+    /// <summary> Tests if this ChangeCount is considered equal to another. </summary>
+    /// <param name="other"> The change count to compare to this object. </param>
+    /// <returns> true if the objects are considered equal, false if they are not. </returns>
+    public bool Equals(SnapshotToken other)
+    {
+      return _changeCount == other._changeCount;
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj))
+        return false;
+
+      return obj is SnapshotToken && Equals((SnapshotToken) obj);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+      return _changeCount;
+    }
+
+    /// <summary> Equality operator. </summary>
+    /// <param name="left"> The left. </param>
+    /// <param name="right"> The right. </param>
+    /// <returns> True if they are equal, false otherwise. </returns>
+    public static bool operator ==(SnapshotToken left, SnapshotToken right)
+    {
+      return left.Equals(right);
+    }
+
+    /// <summary> Inequality operator. </summary>
+    /// <param name="left"> The left. </param>
+    /// <param name="right"> The right. </param>
+    /// <returns> True if they are in-equal, false otherwise. </returns>
+    public static bool operator !=(SnapshotToken left, SnapshotToken right)
+    {
+      return !left.Equals(right);
+    }
+  }
+}
---
+++ Assets/BringBackSociety/Scripts/FireableWeaponBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BringBackSociety.Items;
+using BringBackSociety.Items.Weapons;
+using UnityEngine;
+
+namespace Models
+{
+  /// <summary> Represents a weapon which can be fired. </summary>
+  public class FireableWeaponBehavior : ExtendedBehaviour, IModelProvider<IFireableWeaponModel>
+  {
+    private Implementation _implementation;
+
+    /// <inheritdoc />
+    IFireableWeaponModel IModelProvider<IFireableWeaponModel>.ModelImplementation
+    {
+      get { return _implementation; }
+    }
+
+    public void Awake()
+    {
+      _implementation = new Implementation(gameObject, this);
+    }
+
+    /// <summary> The actual implementation of the given model. </summary>
+    private class Implementation : BaseModel<FireableWeaponBehavior, IFireableWeaponModel>,
+                                   IFireableWeaponModel
+    {
+      private readonly Light _light;
+      private readonly AudioSource _audio;
+
+      private FireableWeaponState _state;
+
+      public Implementation(GameObject gameObject, FireableWeaponBehavior behavior)
+        : base(gameObject, behavior)
+      {
+        _light = gameObject.GetComponentsInChildren<Light>().First();
+        _audio = gameObject.GetComponentsInChildren<AudioSource>().First();
+
+        _state = FireableWeaponState.None;
+      }
+
+      /// <inheritdoc />
+      public FireableWeapon FireableWeapon { get; set; }
+
+      /// <inheritdoc />
+      public bool CanEnterState(FireableWeaponState state)
+      {
+        return _state == FireableWeaponState.None;
+      }
+
+      /// <inheritdoc />
+      public void TransitionToState(FireableWeaponState state)
+      {
+        Behavoir.StartCoroutine(FireWeapon());
+      }
+
+      private IEnumerator FireWeapon()
+      {
+        _state = FireableWeaponState.Fired;
+
+        var light = _light;
+        var audio = _audio;
+
+        light.enabled = true;
+        audio.Play();
+
+        yield return new WaitForSeconds(0.05f);
+        light.enabled = false;
+
+        _state = FireableWeaponState.None;
+      }
+    }
+  }
+}
