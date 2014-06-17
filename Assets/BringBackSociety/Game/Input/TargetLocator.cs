b172e6f08e4f2380c4a29d7b7431c34ca6f2d709
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2014-06-16 22:28:26 -0500

    Add current tile indicator.
    [fix] World to Vector3 position conversions issues.

--- Assets/BringBackSociety/Game/Input/TargetLocator.cs
+++ Assets/BringBackSociety/Game/Input/TargetLocator.cs
@@ -15,6 +15,9 @@ namespace BringBackSociety.Game.Input
     /// <summary> The inner radius where mouse movement does not count </summary>
     private readonly int _innerRadius;
 
+    /// <summary> The center of the screen. </summary>
+    private readonly Vector2 _center;
+
     /// <summary> Constructor. </summary>
     /// <param name="width"> The width of the screen. </param>
     /// <param name="height"> The height of the screen. </param>
@@ -22,7 +25,7 @@ namespace BringBackSociety.Game.Input
     /// <param name="outerRadius"> The outer radius where mouse movement does count. </param>
     public TargetLocator(int width, int height, int innerRadius, int outerRadius)
     {
-      Center = new Vector2(width / 2.0f, height / 2.0f);
+      _center = new Vector2(width / 2.0f, height / 2.0f);
 
       _innerRadius = innerRadius;
       _distanceToOuterRadius = outerRadius - _innerRadius;
@@ -42,14 +45,11 @@ namespace BringBackSociety.Game.Input
     /// </summary>
     public Vector2 CurrentPosition { get; private set; }
 
-    /// <summary> The center of the screen. </summary>
-    public Vector2 Center { get; private set; }
-
     /// <summary> Updates the location of the mouse. </summary>
     /// <param name="location"> The location of the mouse in screen coordinates. </param>
     public void UpdatePosition(Vector2 location)
     {
-      var ray = location - Center;
+      var ray = location - _center;
       float distance = ray.magnitude;
 
       if (distance < _innerRadius)
@@ -67,7 +67,8 @@ namespace BringBackSociety.Game.Input
       CurrentPosition = ray.normalized;
 
       Strength = strength * 100;
-      Direction = Quaternion.FromToRotation(Vector3.forward, new Vector3(ray.x, 0, ray.y));
+      Direction = Quaternion.FromToRotation(Vector3.forward,
+        new Vector3(CurrentPosition.x, 0, CurrentPosition.y));
     }
   }
 }
