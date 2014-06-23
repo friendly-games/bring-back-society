7bc0edf5e042282bd84fadc4bec571d8aa450bca
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2014-06-22 23:10:08 -0500

    Being implementation of proxy objects.

--- Assets/BringBackSociety/Engine/Tile.cs
+++ Assets/BringBackSociety/Engine/Tile.cs
@@ -9,6 +9,9 @@ namespace BringBackSociety
   [StructLayout(LayoutKind.Explicit)]
   public struct Tile
   {
+    /// <summary> An empty tile. </summary>
+    public static Tile Empty;
+
     /// <summary>
     /// The raw type of the tile.
     /// </summary>
@@ -23,6 +26,32 @@ namespace BringBackSociety
     public bool IsEmpty
     {
       get { return Type == 0; }
+    }
+  }
+
+  /// <summary> Represents a specific tile on a specific chunk. </summary>
+  public struct TileReference
+  {
+    /// <summary> The chunk to which this proxy should point to. </summary>
+    private readonly Chunk _chunk;
+
+    /// <summary> The coordinate of the tile that is being observed. </summary>
+    private readonly TileCoordinate _coordinate;
+
+    /// <summary> Constructor. </summary>
+    /// <param name="chunk"> The chunk to which this proxy should point to. </param>
+    /// <param name="coordinate"> The coordinate of the tile that is being observed. </param>
+    public TileReference(Chunk chunk, TileCoordinate coordinate)
+    {
+      _chunk = chunk;
+      _coordinate = coordinate;
+    }
+
+    /// <summary> The tile data of the tile at the given position. </summary>
+    public Tile Value
+    {
+      get { return _chunk[_coordinate]; }
+      set { _chunk[_coordinate] = value; }
     }
   }
 }
---
+++ Assets/BringBackSociety/Game/Items/WallTemplate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BringBackSociety.Items;
+
+namespace BringBackSociety.Game.Items
+{
+  /// <summary> A template that represents a specific type of wall. </summary>
+  public class WallTemplate
+  {
+    /// <summary> Constructor. </summary>
+    /// <param name="name"> The name of the designated wall. </param>
+    /// <param name="maxHealth"> The maximum amount of health that the wall can have. </param>
+    /// <param name="resistance"> The resistance for the wall. </param>
+    public WallTemplate(string name, int maxHealth, Resistance resistance)
+    {
+      if (name == null)
+        throw new ArgumentNullException("name");
+      if (resistance == null)
+        throw new ArgumentNullException("resistance");
+
+      Name = name;
+      Resistance = resistance;
+      MaxHealth = maxHealth;
+    }
+
+    /// <summary> The resistance for the wall. </summary>
+    public Resistance Resistance { get; private set; }
+
+    /// <summary> The maximum amount of health that the wall can have. </summary>
+    public int MaxHealth { get; private set; }
+
+    /// <summary> The name of the designated wall. </summary>
+    public string Name { get; private set; }
+  }
+}
